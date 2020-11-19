2cd058af01967a8249e0b11a8c17be50e54d551d
Christina <chriskreza@csd.auth.gr>
2020-11-19 11:50:49 +0200

    Added bash script to analyze logs

---
+++ bash_scripts/analysis.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo -e "SSH Log Analysis\n" > analysis.txt
+cat logs.txt | grep "Invalid user" > invalidUsers.txt
+awk '{print $8}' invalidUsers.txt | sort | uniq -c | sort -nr | awk '{print $2 "\t" $1}' > usernames.txt
+
+echo "Most used usernames" >> analysis.txt
+head -5 usernames.txt >> analysis.txt
+printf "\n" >> analysis.txt
+rm ./usernames.txt
+
+awk '{print $10}' invalidUsers.txt | sort | uniq -c | sort -nr | awk '{print $2 "\t" $1}' > ips.txt
+rm ./invalidUsers.txt
+
+echo "Most common ips" >> analysis.txt
+head -5 ips.txt >> analysis.txt
+printf "\n" >> analysis.txt
+
+cat ips.txt | xargs -n 1 geoiplookup { } | awk '{print $5}' | sort | uniq -c | sort -nr | awk '{print $2 "\t" $1}' > countries.txt
+rm ./ips.txt
+
+echo "Most common countries" >> analysis.txt
+head -5 countries.txt >> analysis.txt
+rm ./countries.txt
