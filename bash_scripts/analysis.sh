863ca28438b3521333536f2b204686006c3a7a18
Christina <chriskreza@csd.auth.gr>
2021-02-25 15:11:37 +0200

    Updated bash script

--- bash_scripts/analysis.sh
+++ bash_scripts/analysis.sh
@@ -1,24 +1,13 @@
 #!/bin/bash
 
-echo -e "SSH Log Analysis\n" > analysis.txt
-cat $1 | grep "Invalid user" > invalidUsers.txt
-awk '{print $8}' invalidUsers.txt | sort | uniq -c | sort -nr | awk '{print $2 "\t" $1}' > usernames.txt
+figlet Brutus SSH
+echo "Most used usernames"
+cat $1 | grep "Invalid user" | awk '{print $8}' | sort | uniq -c | sort -nr | awk '{print $2 "\t" $1}' | head -5
+printf "\n"
 
-echo "Most used usernames" >> analysis.txt
-head -5 usernames.txt >> analysis.txt
-printf "\n" >> analysis.txt
-rm ./usernames.txt
+echo "Most common ips"
+cat $1 | grep "Invalid user" | awk '{print $10}' | sort | uniq -c | sort -nr | awk '{print $2 "\t" $1}' | head -5
+printf "\n"
 
-awk '{print $10}' invalidUsers.txt | sort | uniq -c | sort -nr | awk '{print $2 "\t" $1}' > ips.txt
-rm ./invalidUsers.txt
-
-echo "Most common ips" >> analysis.txt
-head -5 ips.txt >> analysis.txt
-printf "\n" >> analysis.txt
-
-cat ips.txt | xargs -n 1 geoiplookup { } | awk '{print $5}' | sort | uniq -c | sort -nr | awk '{print $2 "\t" $1}' > countries.txt
-rm ./ips.txt
-
-echo "Most common countries" >> analysis.txt
-head -5 countries.txt >> analysis.txt
-rm ./countries.txt
+echo "Most common countries"
+cat $1 | grep "Invalid user" | awk '{print $10}' | xargs -n1 geoiplookup | awk '{print $5}' | sort | uniq -c | sort -nr | awk '{print $2 "\t" $1}' | head -5
