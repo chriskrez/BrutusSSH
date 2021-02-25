e9e069a4487818b28958746b0a46100ccc6a2e3b
Christina <chriskreza@csd.auth.gr>
2021-02-25 15:44:08 +0200

    Grep file only once

--- bash_scripts/analysis.sh
+++ bash_scripts/analysis.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 figlet Brutus SSH
+lines=`grep "Invalid user" $1`
+
 echo "Most used usernames"
-cat $1 | grep "Invalid user" | awk '{print $8}' | sort | uniq -c | sort -nr | awk '{print $2 "\t" $1}' | head -5
+echo "$lines" | awk '{print $8}' | sort | uniq -c | sort -nr | awk '{print $2 "\t" $1}' | head -5
 printf "\n"
 
 echo "Most common ips"
-cat $1 | grep "Invalid user" | awk '{print $10}' | sort | uniq -c | sort -nr | awk '{print $2 "\t" $1}' | head -5
+echo "$lines" | awk '{print $10}' | sort | uniq -c | sort -nr | awk '{print $2 "\t" $1}' | head -5
 printf "\n"
 
 echo "Most common countries"
-cat $1 | grep "Invalid user" | awk '{print $10}' | xargs -n1 geoiplookup | awk '{print $5}' | sort | uniq -c | sort -nr | awk '{print $2 "\t" $1}' | head -5
+echo "$lines" | awk '{print $10}' | xargs -n1 geoiplookup | awk '{print $5}' | sort | uniq -c | sort -nr | awk '{print $2 "\t" $1}' | head -5
