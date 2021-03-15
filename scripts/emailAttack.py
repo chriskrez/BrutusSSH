20a16023e427c2d93ee526383cc8107319900169
Christina <chriskreza@csd.auth.gr>
2021-03-15 14:53:33 +0200

    Sending basic email and extracting countries

--- scripts/emailAttack.py
+++ scripts/emailAttack.py
@@ -1,7 +1,11 @@
 from datetime import datetime, timedelta
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 import json
 import os.path
 import re
+import requests
+import smtplib
 
 MONTHS = {"Jan":"01", "Feb":"02", "Mar":"03", "Apr":"04", "May":"05", "Jun":"06", "Jul":"07", "Aug":"08", "Sep":"09", "Oct": "10", "Nov": "11", "Dec": "12"}
 
@@ -9,15 +13,17 @@ def readDefaults():
     if os.path.isfile("./emailDefaults.json"):
         with open("emailDefaults.json") as defFile:
             data = json.load(defFile)
-            global LOG_PATH, TIME_WINDOW, THRESHOLD
+            global LOG_PATH, TIME_WINDOW, THRESHOLD, EMAIL_RECEIVER
             LOG_PATH = data["log_path"]
             TIME_WINDOW = int(data["time_window"])
             THRESHOLD = int(data["threshold"])
+            EMAIL_RECEIVER = data["email_receiver"]
     else:
         content = {
             "log_path":"/var/log/auth.log",
             "time_window":"5",
-            "threshold": "50"
+            "threshold": "50",
+            "email_sender": "example@example.com"
         }
 
         print("Please fill the default values in emailDefaults.json")
@@ -28,7 +34,7 @@ def checkAttack():
     with open(LOG_PATH, "r") as logs:
         now = datetime.now() - timedelta(minutes=TIME_WINDOW)
         flag = False
-        counter = 0
+        ips = []
         while True:
             line = logs.readline()
             if not line:
@@ -43,12 +49,28 @@ def checkAttack():
                 else:
                     continue
             if "Invalid user" in line:
-                counter += 1
-        if counter >= THRESHOLD:
+                reg = re.compile("from (.*) port")
+                ips.append(reg.search(line).group(1))
+
+        if len(ips) >= THRESHOLD:
+            post = []
+            for ip in set(ips):
+                post.append({"query": ip, "fields": "country,query"})
+            res = requests.post("http://ip-api.com/batch", json=post)
             sendMail()
 
 def sendMail():
-    pass
+    smtpObj = smtplib.SMTP('localhost')
+    msg = MIMEMultipart()
+    message = """
+    <b> Brutus SSH </b>    
+    """
+
+    msg['From'] = "info@brutus.ml"
+    msg['To'] = EMAIL_RECEIVER
+    msg['Subject'] = "BrutusSSH Attack Notification"
+    msg.attach(MIMEText(message, 'html'))
+    smtpObj.send_message(msg)
 
 if __name__ == "__main__":
     readDefaults()
