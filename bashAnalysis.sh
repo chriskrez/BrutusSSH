21bb0413b540b80864436181898df17bb7febcbc
Christina <chriskreza@csd.auth.gr>
2021-03-17 20:53:13 +0200

    Updated bash script logic

--- bashAnalysis.sh
+++ bashAnalysis.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 figlet Brutus SSH
-lines=`grep "Invalid user" $1`
+regex=".*Failed password for( invalid user)? (.+) from ([[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+).*"
+
+users=`cat $1 | sed -rn "s/$regex/\2|/p"`
+ips=`cat $1 | sed -rn "s/$regex/\3|/p"`
 
 echo "Most used usernames"
-echo "$lines" | awk '{print $8}' | sort | uniq -c | sort -nr | awk '{print $2 "\t" $1}' | head -5
+echo $users | tr "|" "\n" | sort | uniq -c | sort -nr | awk '{print $2 "\t" $1}' | head -5
 printf "\n"
 
 echo "Most common ips"
-echo "$lines" | awk '{print $10}' | sort | uniq -c | sort -nr | awk '{print $2 "\t" $1}' | head -5
+echo $ips | tr "|" "\n" | sort | uniq -c | sort -nr | awk '{print $2 "\t" $1}' | head -5
 printf "\n"
 
 echo "Most common countries"
-echo "$lines" | awk '{print $10}' | xargs -n1 geoiplookup | awk '{print $5}' | sort | uniq -c | sort -nr | awk '{print $2 "\t" $1}' | head -5
+echo $ips | tr "|" "\n" | awk '{print $1}' | xargs -n1 geoiplookup | awk '{print $5}' | sort | uniq -c | sort -nr | awk '{print $2 "\t" $1}' | head -5
