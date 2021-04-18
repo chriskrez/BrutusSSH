620a4c806d08935310fe7bd3643211a55ab96edb
Christina <chriskreza@csd.auth.gr>
2021-04-18 16:29:00 +0300

    Parcing log file only once

--- brutus_app/src/api/controllers/UploadController.js
+++ brutus_app/src/api/controllers/UploadController.js
@@ -1,9 +1,51 @@
-const scripts = require("../scripts/extractData.js");
+const axios = require("axios");
+
+const extractCaptureGroups = (file) => {
+  var matchingCaptureGroups = [];
+  var hours = [];
+  var usernames = [];
+  var ips = [];
+
+  var regexp = new RegExp(
+    ".* (\\d{2}):(\\d{2}):(\\d{2}) .* Failed password for(?: invalid user)? (.+) from (\\d+\\.\\d+\\.\\d+\\.\\d+).*"
+  );
+
+  file = file.toString();
+  const lines = file.split(/\r?\n/);
+  lines.forEach((line) => {
+    if (line.match(regexp)) {
+      const match = line.match(regexp);
+      var hour = match[1];
+      var username = match[4];
+      var ip = match[5];
+
+      hours.push(hour);
+      usernames.push(username);
+      ips.push(ip);
+    }
+  });
+
+  matchingCaptureGroups.push(hours);
+  matchingCaptureGroups.push(usernames);
+  matchingCaptureGroups.push(ips);
+
+  return matchingCaptureGroups;
+};
 
 const graphUsernames = (usernames) => {
+  var countedUsernames = {};
+
+  usernames.forEach((user) => {
+    if (countedUsernames[user]) {
+      countedUsernames[user] += 1;
+    } else {
+      countedUsernames[user] = 1;
+    }
+  });
+
   const data = [];
-  for (var username in usernames) {
-    data.push({ name: username, value: usernames[username] });
+  for (var username in countedUsernames) {
+    data.push({ name: username, value: countedUsernames[username] });
   }
 
   data.sort((a, b) => {
@@ -14,20 +56,46 @@ const graphUsernames = (usernames) => {
 };
 
 const graphIps = (ips) => {
+  var countedIps = {};
+
+  ips.forEach((ip) => {
+    if (countedIps[ip]) {
+      var oldValue = countedIps[ip];
+      countedIps[ip] = oldValue + 1;
+    } else {
+      countedIps[ip] = 1;
+    }
+  });
+
   const data = [];
-  for (var ip in ips) {
-    data.push({ name: ip, value: ips[ip] });
+  for (var ip in countedIps) {
+    data.push({ name: ip, value: countedIps[ip] });
   }
 
   data.sort((a, b) => {
     return b.value - a.value;
   });
 
-  return data.slice(0, 10);
+  return [countedIps, data.slice(0, 10)];
 };
 
 const graphCountries = async (ips) => {
-  const countries = await scripts.extractCountries(ips);
+  var countries = {};
+
+  for (ip in ips) {
+    const record = ips[ip];
+
+    let res = await axios.get("http://www.geoplugin.net/json.gp?ip=" + ip);
+
+    var country = res.data.geoplugin_countryName;
+    if (countries[country]) {
+      var oldValue = countries[country];
+      countries[country] = oldValue + record;
+    } else {
+      countries[country] = record;
+    }
+  }
+
   const data = [];
 
   for (var obj in countries) {
@@ -41,10 +109,20 @@ const graphCountries = async (ips) => {
   return data.slice(0, 10);
 };
 
-const graphHours = async (hours) => {
+const graphHours = (hours) => {
+  var countedHours = {};
+
+  hours.forEach((hour) => {
+    if (countedHours[hour]) {
+      countedHours[hour] += 1;
+    } else {
+      countedHours[hour] = 1;
+    }
+  });
+
   const data = [];
-  for (var hour in hours) {
-    data.push({ name: hour + ":00", value: hours[hour] });
+  for (var hour in countedHours) {
+    data.push({ name: hour + ":00", value: countedHours[hour] });
   }
 
   data.sort((a, b) => {
@@ -57,13 +135,14 @@ const graphHours = async (hours) => {
 module.exports = {
   async upload(req, res) {
     const data = req.files.file.data;
-    const extractedUsernamesAndIps = await scripts.extractUsernamesAndIps(data);
-    const extractedTime = await scripts.extractTime(data);
+    const captureGroups = extractCaptureGroups(data);
 
-    const usernames = graphUsernames(extractedUsernamesAndIps[0]);
-    const ips = graphIps(extractedUsernamesAndIps[1]);
-    const countries = await graphCountries(ips);
-    const hours = await graphHours(extractedTime);
+    const hours = graphHours(captureGroups[0]);
+    const usernames = graphUsernames(captureGroups[1]);
+
+    const ipsResults = graphIps(captureGroups[2]);
+    const ips = ipsResults[1];
+    const countries = graphCountries(ipsResults[0]);
 
     return res.send({
       usernames,
